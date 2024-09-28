964a8391b24bbfecc24818f57caec4234fc05851
Kavil-Jain-514 <kaviljain14@gmail.com>
2024-09-28 08:01:55 -0700

    backend-setup

---
+++ backend/roomitra/src/main/java/dev/kavil/roomitra/RoomitraApplication.java
@@ -0,0 +1,13 @@
+package dev.kavil.roomitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomitraApplication.class, args);
+	}
+
+}
---
+++ backend/roomitra/src/test/java/dev/kavil/roomitra/RoomitraApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kavil.roomitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
