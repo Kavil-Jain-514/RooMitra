502d8ca3e10f3b0db576ca0d99a25cf373c958e6
Kavil-Jain-514 <kaviljain14@gmail.com>
2024-09-28 08:55:05 -0700

    Added MongoDB Connection

--- backend/roomitra/src/main/java/dev/kavil/roomitra/RoomitraApplication.java
+++ backend/roomitra/src/main/java/dev/kavil/roomitra/RoomitraApplication.java
@@ -2,12 +2,20 @@ package dev.kavil.roomitra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RoomitraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoomitraApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hello World!";
 	}
 
 }
