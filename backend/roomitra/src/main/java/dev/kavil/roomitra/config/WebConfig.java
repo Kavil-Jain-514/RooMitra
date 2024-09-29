df0d15856f403f73bd6a78874b2d9353e501fa84
Kavil-Jain-514 <kaviljain14@gmail.com>
2024-09-29 15:31:24 -0700

    connected front and back end of the application

---
+++ backend/roomitra/src/main/java/dev/kavil/roomitra/config/WebConfig.java
@@ -0,0 +1,18 @@
+package dev.kavil.roomitra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Apply to all endpoints
+                .allowedOrigins("http://localhost:3000") // Your React app's origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials if needed
+    }
+}
--- client/roomitra-frontend/src/App.js
+++ client/roomitra-frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
 function App() {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    try {
+      const response = await api.get("/api/v1/users");
+      console.log(response.data);
+      setUsers(response.data);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    getUsers();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map(user => (
+        <div key={user.id}> 
+          <h2>{user.username}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
